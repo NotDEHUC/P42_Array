176ed0227e0fb6ece5ac98bac71bb487af2af49a
student <student@SSD1302>
2025-09-23 19:21:21 +0300

    finish this something wrong

--- P42_Array/P42_Array.cpp
+++ P42_Array/P42_Array.cpp
@@ -1,20 +1,152 @@
-﻿// P42_Array.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿#include <iostream>
 
-#include <iostream>
+using namespace std;
+
+class Array
+{
+    int length;
+    int* arr;
+
+public:
+    Array(int len) : arr{ new int[len] }, length{len} {}
+    Array() : Array(0) {}
+
+    Array(const Array& obj)
+    {
+        length = obj.length;
+
+        if (obj.arr)
+        {
+            for (size_t i = 0; i < length; i++)
+            {
+                *(arr + i) = *(obj.arr + i);
+            }
+        }
+        else
+        {
+            arr = new int[obj.length];
+        }
+    }
+
+    ~Array()
+    {
+        delete[]arr;
+    }
+
+
+    void setLen(int length)
+    {
+        this->length = length;
+        if (arr)
+        {
+            int* a = new int[length];
+            for (size_t i = 0; i < length; i++)
+            {
+                *(a + i) = *(arr + i);
+            }
+
+            delete[]arr;
+
+            arr = new int[length];
+
+            for (size_t i = 0; i < length; i++)
+            {
+                for (size_t i = 0; i < length; i++)
+                {
+                    *(arr + i) = *(a + i);
+                }
+            }
+
+            delete[]a;
+        }
+        else
+        {
+            arr = new int[length];
+        }
+    }
+
+    void fillArr(int* arr)
+    {
+        for (size_t i = 0; i < length; i++)
+        {
+            *(this->arr + i) = *(arr + i);
+        }
+    }
+
+    void print()
+    {
+        for (size_t i = 0; i < length; i++)
+        {
+            cout << *(arr + i) << ' ';
+        }
+    }
+
+    void sortArr()
+    {
+        int temp;
+
+        for (size_t i1 = 0; i1 < length; i1++)
+        {
+            for (size_t i = 0; i < length - 1; i++)
+            {
+                if (*(arr + i) > *(arr + i + 1))
+                {
+                    temp = *(arr + i);
+                    *(arr + i) = *(arr + i + 1);
+                    *(arr + i + 1) = temp;
+                }
+            }
+        }
+        
+        
+    }
+
+    int arrMin()
+    {
+        int min = *(arr + 0);
+
+        for (size_t i = 0; i < length; i++)
+        {
+            if (min > *(arr + i))
+            {
+                min = *(arr + i);
+            }
+        }
+
+        return min;
+    }
+
+    int arrMax()
+    {
+        int max = *(arr + 0);
+
+        for (size_t i = 0; i < length; i++)
+        {
+            if (max < *(arr + i))
+            {
+                max = *(arr + i);
+            }
+        }
+
+        return max;
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Array a(10), a2(a);
+
+    int arr[10]{ 10,1,9,2,8,3,7,4,6,5 };
+    int arr2[10]{ 10,11,19,12,18,13,17,14,16,15 };
+
+    a.fillArr(arr);
+    a2.fillArr(arr2);
+    a.print();
+    cout << endl;
+    a2.print();
+    cout << endl;
+    a.print();
+    cout << endl;
+    cout << a.arrMin() << endl;
+    cout << a.arrMax() << endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
